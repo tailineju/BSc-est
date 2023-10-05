e367ad5dfce4a80168ff360e05f623a61f91d192
Tailine Nonato <56554634+tailineju@users.noreply.github.com>
2023-10-04 22:05:41 -0300

    1st seminar content

--- EC/1st mini seminar/314.R
+++ EC/1st mini seminar/314.R
@@ -1,20 +1,21 @@
 if (!require(pacman)) install.packages("pacman")
-pacman::p_load(tidyverse)
+pacman::p_load(tidyverse,GGally,iris)
 
 rmvn.Choleski <-
-function(n, mu, Sigma) {
+function(n, mu, sigma) {
 # generate n random vectors from MVN(mu, Sigma)
 # dimension is inferred from mu and Sigma
 d <- length(mu)
-Q <- chol(Sigma) # Choleski factorization of Sigma
+Q <- chol(sigma) # Choleski factorization of Sigma
 Z <- matrix(rnorm(n*d), nrow=n, ncol=d)
 X <- Z %*% Q + matrix(mu, n, d, byrow=TRUE)
-X
+return(X)
 }
 
 n <- 200
 mu <- c(0,1,2)
 sigma <- matrix(c(1,-.5,.5,-.5,1,-.5,.5,-.5,1),nrow=3)
 x<-rmvn.Choleski(n,mu,sigma)
-
+head(x)
 pairs(x)
+ggpairs(x)
