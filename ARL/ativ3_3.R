8c23bf277cc2fd5cea407b6b388c8c375478c16e
tailineju <190038144@aluno.unb.br>
2023-11-05 15:38:29 -0300

    update reg

---
+++ ARL/ativ3_3.R
@@ -0,0 +1,35 @@
+x <- matrix(c(1,1,1,1,1,8,4,0,-4,-8),5,2)
+y <- matrix(c(7.8,9,10.2,11, 11.7),5,1)
+
+#a
+yty <- t(y)%*%y
+
+xtx <- t(x)%*%x
+
+xty <- t(x)%*%y
+
+#b
+xtxi <- solve(xtx)
+
+#c
+beta <- xtxi %*% xty
+
+#d
+yest <- x %*% beta
+
+ei <- y - yest
+
+#e
+SSE <- yty - (t(beta) %*% t(x) %*% y)
+
+um <- matrix(rep(1,5),5,1)
+umumt <- um%*%t(um)
+
+n <- 5
+
+SSTO <- yty - ((1/n)%*% t(y) %*% umumt %*% y)
+
+SSR <- SSTO - SSE
+
+
+#f
